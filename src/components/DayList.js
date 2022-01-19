94588c4dffffadf3698a662c4cc2695138541624
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-19 09:08:44 -0500

    refactor InterviewerList and InterviewerListItem

---
+++ src/components/DayList.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import DayListItem from "./DayListItem";
+
+
+export default function DayList(props) {
+  //const [day, setDay] = useState('');
+  // console.log(props);
+  // const days = [
+  //   {
+  //     id: 1,
+  //     name: "Monday",
+  //     spots: 2,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Tuesday",
+  //     spots: 5,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Wednesday",
+  //     spots: 0,
+  //   },
+  // ];
+
+  const parsedDayListItem = props.days.map(day => <DayListItem key={day.id}
+    name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay}
+  />);
+  return (
+    <ul>
+      {parsedDayListItem}
+    </ul>
+  );
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,20 @@ export default function DayListItem(props) {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return 'no spots remaining';
+    } else if (props.spots === 1) {
+      return '1 spot remaining'
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  }
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
