822e93c8dcea48833589d28fc972dc41f4dec28b
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-19 19:40:25 -0500

    add Form components and send props to index

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent('');
+    setInterviewer(null);
+
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form
+          onSubmit={event => event.preventDefault()}
+          autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+              your code goes here
+            */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          /* your code goes here */
+          value={interviewer}
+          onChange={(id) => setInterviewer(id)}
+          interviewers={props.interviewers}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel} >Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
